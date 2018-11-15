9c1deffcd9c5e65717875e4b63b6292db0591ff8
Michael <michael@frechtling.net>
2018-11-15 22:55:25 +1100

    Completed implementation

--- theOdds.py
+++ theOdds.py
@@ -11,20 +11,31 @@ def getData(key, sport):
     response = requests.get("https://api.the-odds-api.com/v3/odds", params=parameters)
     return response.json()
 
-def parseJsonData(data):
+def writeCsvData(fileName, data):
+    f = open(fileName, "w")
+    f.write("home, away, home_odds, away_odds\n")
     for game in data["data"]:
-        print(game["home_team"])
+        if (game["teams"][0] ==  game["home_team"]):
+            home_index = 0
+            away_index = 1
+        else:
+            home_index = 1
+            away_index = 0
         for site in game["sites"]:
-            print(site["site_key"])
-            print(site["odds"]["h2h"][0])
-            print(site["odds"]["h2h"][1])
+            if (site["site_key"] != "sportsbet"):
+                continue
+            f.write(game["teams"][home_index] + "," + game["teams"][away_index] + "," + str(site["odds"]["h2h"][home_index]) + "," + str(site["odds"]["h2h"][away_index]) + "\n")
+    f.close()
 
 def main():
     key = getApiKey()
-    #results = getData(key, 'basketball_nba')
+    results = getData(key, 'basketball_nba')
+    f = open("results.json", "w")
+    f.write(json.dumps(results))
+    f.close()
     f = open("results.json", "r")
     results = json.loads(f.read())
-    parseJsonData(results)
+    writeCsvData("./results.csv", results)
 
 
 if __name__ == '__main__':
